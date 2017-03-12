9034c31cbed3ee72eba938c26202badf7cb7206d
Tusko <tusko@photoinside.me>
2017-03-12 09:35:55 +0200

    Hash tracking added

--- init.js
+++ init.js
@@ -1,17 +1,24 @@
 jQuery(document).ready(function($){
-    $('form').on('change', 'input', function(){
-        calc_rate();
+    $('form').on('change keyup', 'input', function(){
+        calc_rate($(this).parents('form'));
     });
+    if(location.hash) {
+        var hashes = location.hash.substring(1).split('&');
+        $.each(hashes, function(i, v){
+            var parseHash = v.split('=');
+            $('input[name="' + parseHash[0] + '"]').val(parseHash[1]);
+        });
+        calc_rate();
+    }
 });
 
-function calc_rate(){
+function calc_rate(form){
     'use strict';
     var hRate      = parseInt($('input#hourly-rate').val(), 10) / 60,
         minutes    = parseInt($('input#hours').val() * 60) + Number($('input#minutes').val()),
         result_id  = $('#result span');
-    if(hRate.length < 1) {
-        alert('Enter hourly rate');
+    if(form) {
+        location.hash = form.serialize();
     }
-    console.log(minutes);
-    result_id.text( parseFloat(hRate * minutes) );
+    result_id.text( parseFloat(hRate * minutes).toFixed(2) );
 }
