a738fa339be76005a6f08ebf4ae1d197ebfb25a1
Tusko <tusko@photoinside.me>
2019-07-23 09:21:11 +0300

    Added currency switcher

--- src/components/rateForm.vue
+++ src/components/rateForm.vue
@@ -2,59 +2,77 @@
   <form action>
     <p>
       <label>Enter your hourly rate</label>
-      <number-input
-        v-model="form.rate"
-        size="large"
-        :min="1"
-        center
-        controls
-      ></number-input>
+      <number-input v-model="form.rate" size="large" :min="1" center controls></number-input>
     </p>
     <p>
       <label>Enter hours worked</label>
-      <number-input
-        v-model="form.h"
-        size="large"
-        :min="1"
-        center
-        controls
-      ></number-input>
+      <number-input v-model="form.h" size="large" :min="1" center controls></number-input>
     </p>
     <p>
       <label for="minutes">Enter minutes worked</label>
-      <number-input
-        v-model="form.m"
-        size="large"
-        :max="60"
-        center
-        controls
-      ></number-input>
+      <number-input v-model="form.m" size="large" :max="60" center controls></number-input>
+    </p>
+    <p class="price-converter">
+      <input class="apple-switch" id="convertToUAH" type="checkbox" v-model="convertToUAH" />
+      <label for="convertToUAH">Convert to UAH</label>
     </p>
     <mark>
-      $
-      <span>{{ calcTotals }}</span>
+      {{ convertToUAH?'₴':'$' }}
+      <span>{{ convertToUAH ? calcTotalsUAH : calcTotals }}</span>
     </mark>
   </form>
 </template>
 
+<style lang="scss">
+.price-converter {
+  align-items: center;
+  display: flex;
+  label {
+    margin: 0 0 0 10px;
+    font-size: 18px;
+  }
+}
+</style>
+
+
 <script>
 import Vue from "vue";
 import VueNumberInput from "@chenfengyuan/vue-number-input";
+import { Checkbox } from "vue-checkbox-radio";
+import "@/assets/ios-input.scss";
+
+Vue.component();
 
 Vue.use(VueNumberInput);
 
 export default {
   name: "HourlyRateForm",
+  components: {
+    checkbox: Checkbox
+  },
   data: () => ({
     form: {
       rate: 0,
       h: 0,
       m: 0
-    }
+    },
+    convertToUAH: false,
+    exchange: null
   }),
   beforeMount() {
     if (this.$route.query.s) {
       this.form = JSON.parse(this.$route.query.s);
+    }
+    let privat24Exchange = sessionStorage.getItem("exchange");
+    if (privat24Exchange) {
+      this.exchange = JSON.parse(privat24Exchange);
+    } else {
+      fetch("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5")
+        .then(r => r.json())
+        .then(json => {
+          this.exchange = json;
+          sessionStorage.setItem("exchange", JSON.stringify(json));
+        });
     }
   },
   computed: {
@@ -64,6 +82,10 @@ export default {
     calcTotals() {
       const totals = (this.form.rate / 60) * this.time2minites;
       return !isNaN(totals) ? totals.toFixed(2) : 0;
+    },
+    calcTotalsUAH() {
+      const totalsUAH = this.calcTotals * Number(this.exchange[0].sale);
+      return !isNaN(totalsUAH) ? totalsUAH.toFixed(2) : 0;
     }
   },
   methods: {
