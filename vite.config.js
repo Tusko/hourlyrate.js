6a9214d3228a01e0e4a06f52ee7830af40dd4b2f
Tusko <tusko@photoinside.me>
2022-02-06 15:55:15 +0200

    fx lint

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import {defineConfig, loadEnv} from 'vite'
-import vue from '@vitejs/plugin-vue'
-import zip from 'rollup-plugin-zip'
-import copy from 'rollup-plugin-copy'
-const path = require('path');
+import {defineConfig, loadEnv} from "vite";
+import vue from "@vitejs/plugin-vue";
+import zip from "rollup-plugin-zip";
+import copy from "rollup-plugin-copy";
+const path = require("path");
 
 export default ({mode}) => {
   process.env = {...process.env, ...loadEnv(mode, process.cwd())};
@@ -10,29 +10,29 @@ export default ({mode}) => {
     plugins: [vue()],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '/src'),
+        "@": path.resolve(__dirname, "/src"),
       },
     },
   };
 
   if (+process.env.VITE_APP_IS_CHROME) {
-    console.log('prepare Chrome extension build')
+    console.log("prepare Chrome extension build");
     config.build = {
-      outDir: './dist-chrome'
-    }
+      outDir: "./dist-chrome",
+    };
     config.plugins.push(
       copy({
-        targets: [
-          {src: 'public-chrome/*', dest: 'dist-chrome'},
-        ],
+        targets: [{src: "public-chrome/*", dest: "dist-chrome"}],
         verbose: true,
-        hook: 'writeBundle'
+        hook: "writeBundle",
       })
-    )
-    config.plugins.push(zip({
-      dir: 'dist-chrome'
-    }))
+    );
+    config.plugins.push(
+      zip({
+        dir: "dist-chrome",
+      })
+    );
   }
 
-  return defineConfig(config)
-}
+  return defineConfig(config);
+};
